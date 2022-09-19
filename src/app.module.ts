b07b9db4b38679b585778b3bf300798a40d3ecaf
giovany.perez <giovany.perez@technipfmc.com>
2022-09-19 04:10:11 -0500

    configure conection with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
+import {APP_INTERCEPTOR} from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {ConfigModule} from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        TTL:180
+      }),
+      isGlobal: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -21,6 +34,12 @@ import { AuthModule } from './auth/auth.module';
     AuthModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor
+    },
+    AppService
+  ],
 })
 export class AppModule {}
